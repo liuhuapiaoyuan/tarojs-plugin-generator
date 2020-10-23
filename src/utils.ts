e2d6176c804dc900ea1d839a3a0d02ab191bf80c
流花飘原 <guohl1990@126.com>
2020-10-23 15:28:09 +0800

    修正首字母大写函数错误

--- src/utils.ts
+++ src/utils.ts
@@ -1,6 +1,7 @@
+/** 首字母大写 */
 export function firstUpperCase(str:string){
     return str.replace(/\b(\w)(\w*)/g, function(_$0, $1, $2) {
-        return $1.toUpperCase() + $2.toLowerCase();
+        return $1.toUpperCase() + $2;
     });
   }
 
